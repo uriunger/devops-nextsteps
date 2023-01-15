3b7059dac69c48dc2b383fac7ae343ab3c0f2c0b
Uri Unger <uriunger@opsp.me>
2023-01-15 00:35:05 -0800

    m

--- src/main/java/DbAccessor.java
+++ src/main/java/DbAccessor.java
@@ -1,26 +1,37 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DbAccessor {
 
-    // db is provisioned with a single table called access with this schema:
-    // create table access (id int primary key auto_increment, time datetime not null);
 
-    public static void logAccess() throws SQLException, ClassNotFoundException {
+    public static void logAccess(String clientIpAddress) throws SQLException, ClassNotFoundException {
+
+        // load jdbc driver for mysql
         Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // url for database should be set as ENV variable
         String url = System.getenv("JDBC_URL");
+
         try (Connection con = DriverManager
                 .getConnection(url, "uri", "TopSecret#0")) {
 
             con.setAutoCommit(false);
 
+            // automatically create schema if missing
+            // db should be provisioned with a single table called 'access' with this schema:
+            // create table access (id int primary key auto_increment, time datetime not null);
             try (Statement stmt = con.createStatement()) {
+                String createTable = "create table access (" +
+                        "id int primary key auto_increment, " +
+                        "time datetime not null," +
+                        "caller_ip string not null);";
+                stmt.execute(createTable);
+                con.commit();
+            }
 
-                String insertSql = "insert into access (time) values (current_timestamp)";
-                stmt.executeUpdate(insertSql);
-
+            String insertSql = "insert into access (time, caller_ip) values (current_timestamp, ?)";
+            try (PreparedStatement stmt = con.prepareStatement(insertSql)) {
+                stmt.setString(1, clientIpAddress);
+                stmt.executeUpdate();
                 con.commit();
                 con.close();
             }
